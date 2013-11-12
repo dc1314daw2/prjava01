96946ddb51367999e96a7ec80853e21fd6495dd3
Daniel Collados <daw2@daw2-m08.fjeclot.net>
2013-11-12 12:42:50 +0100

    versió 201311121243

---
+++ README.txt
@@ -0,0 +1,4 @@
+Treballant amb instruccions GIT
+Versió 0.1
+versió 0.2
+
--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -25,7 +25,7 @@ public class Prjava01 {
             bw.write("  <body>");bw.newLine();
             bw.write("    <b>daw2-m08uf4</b><br>");bw.newLine();
             bw.write("    <b>Curs 2013-2104</b><br>");bw.newLine();
-            bw.write("    Nova p&agrave;gina web");bw.newLine();
+            bw.write("    Nova p&agrave;gina web v0.1");bw.newLine();
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
             bw.close();
